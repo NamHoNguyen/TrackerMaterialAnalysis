953cde0ca310a43014208a1e1f136ef2b133d78f
Ho Nam Nguyen <nam.h.nguyen@stonybrook.edu>
2017-07-18 17:20:19 +0200

    no more negative radlength for TID+2, still some for separate triplets due to lack of statistics, MatPlotter_2D works for separate triplets now

--- MatPlotter_2D.py
+++ MatPlotter_2D.py
@@ -1,3 +1,10 @@
+'''
+Author: Ho Nam Nguyen, Elisabetta Manca
+Purpose:
+- This script takes in a 3D-sagitta file and calculates corresponding radiation length
+- The result is binned in local eta and local phi
+- It plots out fits with negative radiaton lengths
+'''
 import ROOT
 import math
 import time
@@ -11,8 +18,8 @@ ROOT.gStyle.SetOptFit()
 
 #prefix = 'July13_3files_6binPhi_10binPt2_'
 #source = 'MC'
-prefix = 'July11_allfiles_6binPhi_10binPt2_'
-#prefix = 'July5_allfiles_6binPhi_10binPt2_'
+#prefix = 'July11_allfiles_6binPhi_10binPt2_'
+prefix = 'July5_allfiles_6binPhi_10binPt2_'
 source = 'DATA'
 
 # Important variables
@@ -41,51 +48,53 @@ for key in allKey:
     # When switch region or at the end of 'minus' regions
     if ((triplet_old!=triplet) and (triplet_old!='')) or (key == allKey[len(allKey)/2-1]):
         '''
-        if (triplet_old!='TIB1'):
-            triplet_old = triplet
-            continue
+        try:
+            if (triplet_old.split('_')[1]!='pixeldisk+1'):
+                triplet_old = triplet
+                continue
+        except:
+            if (triplet_old!='pixeldisk+1'):
+                triplet_old = triplet
+                continue
         '''
-        th3m = [fIn.Get(x) for x in allKey if (triplet_old in x) and ('minus' in x)]
-        th3pl = [fIn.Get(x) for x in allKey if (triplet_old in x) and ('plus' in x)]
-        #print th3m,th3pl
+        
+        th3m = [fIn.Get(x) for x in allKey if (triplet_old in x) and ('minus' in x) and (x.split('_')[-1].replace('.','').isdigit())]
+        th3pl = [fIn.Get(x) for x in allKey if (triplet_old in x) and ('plus' in x) and (x.split('_')[-1].replace('.','').isdigit())]
+        th3m_key = [x for x in allKey if (triplet_old in x) and ('minus' in x) and (x.split('_')[-1].replace('.','').isdigit())]
+        th3pl_key = [x for x in allKey if (triplet_old in x) and ('plus' in x) and (x.split('_')[-1].replace('.','').isdigit())]
         # define TH2 to fill
         slope=ROOT.TH2D("slope"+triplet_old, "slope"+triplet_old, 41, -4, 4, nphi, phimin, phimax)
-        radlength=ROOT.TH2D("radlength"+triplet_old, "radlength"+triplet_old, 41, -4, 4, nphi, phimin, phimax)
+        radlength=ROOT.TH2D("radlength_"+triplet_old, "radlength_"+triplet_old, 41, -4, 4, nphi, phimin, phimax)
 
         for i in range(1, th3m[0].GetNbinsX()+1):
             for j in range(1, th3m[0].GetNbinsY()+1):
                 rms=ROOT.TH1D("{i}_{j}".format(i=i,j=j),"{i}_{j}".format(i=i,j=j), npt2, pt2min, pt2max);
                 #rms=ROOT.TH1D("{i}_{j}".format(i=i,j=j),"{i}_{j}".format(i=i,j=j), 20, 0.5, 2.25);
                 # loop over pt
-                if len(th3m)!=len(th3pl): print len(th3m),len(th3pl)
                 for k in range(len(th3m)):
-                    try:
-                        th1m = ROOT.TH1
-                        th1pl = ROOT.TH1
-                        th1m = th3m[k].ProjectionZ("binMinus_{i}_{j}".format(i=i,j=j), i, i, j, j)
-                        th1pl = th3pl[k].ProjectionZ("binPlus_{i}_{j}".format(i=i,j=j), i, i, j, j)
+                    th1m = ROOT.TH1
+                    th1pl = ROOT.TH1
+                    th1m = th3m[k].ProjectionZ("binMinus_{i}_{j}".format(i=i,j=j), i, i, j, j)
+                    # check if the corresponding plus histogram exist
+                    try: kpl = th3pl_key.index(th3m_key[k].replace('sag3Dminus_','sag3Dplus_'))
                     except:
-                        break
+                        print 'No corresponding plus histogram'
+                        continue
+                    th1pl = th3pl[kpl].ProjectionZ("binPlus_{i}_{j}".format(i=i,j=j), i, i, j, j)
 
                     pt = float([p for p in th3m[k].GetName().split('_') if not 'minus' in p][-1])
-                    ptpl = float([p for p in th3pl[k].GetName().split('_') if not 'plus' in p][-1])
-                    if abs(pt-ptpl)>1.e-3:
-                        print '---------------> Errorrrrr! Pts do not match!'
-                        print th3m
-                        print th3pl
-                        sys.exit()
 
                     mean=(1./2*(th1m.GetRMS()+th1pl.GetRMS()))
                     mean_err=mean*math.sqrt(th1m.GetRMSError()*th1m.GetRMSError()+th1pl.GetRMSError()*th1pl.GetRMSError())
                     
-                    #if mean!=0.: print mean,mean_err
                     if abs(mean*mean)>1.e-10:
                     #if abs(mean*mean)>1.e-6:
                         rms.SetBinContent(rms.FindBin(pt), mean*mean)
                         rms.SetBinError(rms.FindBin(pt), mean_err)
+                        if abs(mean*mean)<1.e-6:
+                            print triplet,i,j
 
-                #if (radlength.GetXaxis().GetBinCenter(i)+0.2)<1.e-2: print rms.GetEntries(),radlength.GetYaxis().GetBinCenter(j)
-                if(rms.GetEntries()<7): continue
+                if(rms.GetEntries()<7):  continue
                 fit=ROOT.TF1("fit", "pol1");
                 rms.Fit(fit, "Q");
 
@@ -98,17 +107,17 @@ for key in allKey:
                     print 'Negative RadLength: ',eta,phi,fit.GetParameter(0),rms.GetEntries()
                     rms.Draw()
                     #rms.GetYaxis().SetRangeUser(0,12e-6)
-                    canv.SaveAs('plots/'+prefix+'negativeTest_'+triplet_old+'_eta'+str(round(eta,2))+'_phi'+str(round(phi,2))+'.png')
+                    canv.SaveAs(prefix+'negativeTest_'+triplet_old+'_eta'+str(round(eta,2))+'_phi'+str(round(phi,2))+'.png')
                 '''
                 else:
                     eta = radlength.GetXaxis().GetBinCenter(i)
                     phi = radlength.GetYaxis().GetBinCenter(j)
-                    if abs(eta+0.2)<1.e-2:
-                    #if True:
+                    #if abs(eta+0.2)<1.e-2:
+                    if True:
                         rms.Draw()
                         #rms.GetYaxis().SetRangeUser(0,20e-6)
-                        canv.SaveAs('plots/'+prefix+'positiveTest_'+triplet_old+'_eta'+str(round(eta,2))+'_phi'+str(round(phi,2))+'.png')
-                '''
+                        canv.SaveAs(prefix+'positiveTest_'+triplet_old+'_eta'+str(round(eta,2))+'_phi'+str(round(phi,2))+'.png')
+                '''     
                 radlength.SetBinContent(i,j,fit.GetParameter(0)/0.013/0.013)
                 radlength.SetBinError(i,j,fit.GetParError(0)/0.013/0.013)
                 radlength.GetXaxis().SetTitle("local #eta")
@@ -119,73 +128,3 @@ for key in allKey:
     triplet_old = triplet
     #if (key == allKey[10]): break
 print("--- %s seconds ---" % (time.time() - start_time))
-'''
-start_time = time.time()
-# loop over histograms
-allKey = fIn.GetListOfKeys()
-print allKey[len(allKey)/2-1].GetName()
-for key in allKey:
-
-    if not 'minus' in key.GetName(): continue
-    print key.GetName()
-
-    # take only the minus histograms fro the list
-    th3=ROOT.TH3
-    th3=fIn.Get(key.GetName())
-    keyArray = [p for p in th3.GetName().split('_') if not 'minus' in p]
-    triplet = keyArray[0]
-    
-    if ((triplet_old!=triplet) and (triplet_old!='')) or (key.GetName() == allKey[len(allKey)/2-1].GetName()):
-        print [x.GetName() for x in fIn.GetListOfKeys() if (triplet_old in x.GetName()) and ('minus' in x.GetName())]
-        # define TH2 to fill
-        slope=ROOT.TH2D("slope"+triplet_old, "slope"+triplet_old, 41, -4, 4, 65, -3.2, 3.2)
-        radlength=ROOT.TH2D("radlength"+triplet_old, "radlength"+triplet_old, 41, -4, 4, 65, -3.2, 3.2)
-
-        for i in range(1, th3.GetNbinsX()+1):
-            for j in range(1, th3.GetNbinsY()+1):
-                rms=ROOT.TH1D("{i}_{j}".format(i=i,j=j),"{i}_{j}".format(i=i,j=j), 10, 0.5, 2.25);
-                #print [x.GetName() for x in fIn.GetListOfKeys() if (triplet_old in x.GetName()) and ('minus' in x.GetName())]
-                #continue
-                for name in [x.GetName() for x in fIn.GetListOfKeys() if (triplet_old in x.GetName()) and ('minus' in x.GetName())]:
-                    # take only the minus histograms fro the list
-                    th3m = ROOT.TH3
-                    th3m = fIn.Get(name)
-                    keyArray = [p for p in th3.GetName().split('_') if not 'minus' in p]
-                    # find the paired plus
-                    th3pl=ROOT.TH3
-                    th3pl=fIn.Get('sag3Dplus_'+'_'.join(keyArray))
-                    
-                    #print th3.GetName(), th3pl.GetName()
-                    #continue
-                    pt = float(keyArray[1])
-
-                    th1m = ROOT.TH1
-                    th1pl = ROOT.TH1
-                    th1m = th3m.ProjectionZ("binMinus_{i}_{j}".format(i=i,j=j), i, i, j, j)
-                    th1pl = th3pl.ProjectionZ("binPlus_{i}_{j}".format(i=i,j=j), i, i, j, j)
-
-                    mean=(1./2*(th1m.GetRMS()+th1pl.GetRMS()))
-                    mean_err=mean*math.sqrt(th1m.GetRMSError()*th1m.GetRMSError()+th1pl.GetRMSError()*th1pl.GetRMSError())
-
-                    rms.SetBinContent(rms.FindBin(pt), mean*mean)
-                    rms.SetBinError(rms.FindBin(pt), mean_err)
-
-                if(rms.GetEntries()<7): continue
-                fit=ROOT.TF1("fit", "pol1");
-                rms.Fit(fit, "Q");
-                
-                slope.SetBinContent(i,j,fit.GetParameter(1))
-                slope.SetBinError(i,j,fit.GetParError(1))
-                
-                radlength.SetBinContent(i,j,fit.GetParameter(0)/0.013/0.013)
-                #radlength.SetBinError(i,j,fit.GetParError(0)/0.013/0.013)
-            
-                #radlength.GetXaxis().SetTitle("local #eta")
-                #radlength.GetYaxis().SetTitle("x/X_{0}")
-        radlength.Draw('colz')    
-        canv.SaveAs('out.png')                       
-        radlength.Write()
-    triplet_old = triplet
-    if (key.GetName() == allKey[30].GetName()): break
-print("--- %s seconds ---" % (time.time() - start_time))
-'''
--- projectRadLength.py
+++ projectRadLength.py
@@ -1,14 +1,17 @@
-import numpy as np
+'''
+Author: Ho Nam Nguyen
+Purpose: 
+- This script projects DATA and MC 2D-RadLength onto phi-direction for fitting.
+- It plots DATA and MC together and separately with the fits
+- It only saves plots with high chi2 or high fluctuation amplitude or zero-ndf for further analysis
+'''
+
 import matplotlib.pyplot as plt
 import ROOT
-import math
 import time
 import sys
 from ROOT import TColor
-#from ROOT import gStyle
 ROOT.gStyle.SetPalette(51)
-#TColor.InvertPalette()
-
 
 prefixMC = 'July13_3files_6binPhi_10binPt2_'
 #source = 'MC'
@@ -17,20 +20,16 @@ prefix = 'July5_allfiles_6binPhi_10binPt2_'
 
 fInMC=ROOT.TFile(prefixMC+'RadLength2D_MC.root')
 fIn=ROOT.TFile(prefix+'RadLength2D_DATA.root')
-#fIn=ROOT.TFile(prefix+'RadLength2D_DATA.root')
-#fOut=ROOT.TFile(prefix+'RadLength2D_'+source+'.root', 'RECREATE')
 
 ROOT.gStyle.SetOptStat(0)
 ROOT.gROOT.SetBatch(ROOT.kTRUE)
-
-triplet_old = ''
 
 start_time = time.time()
 # loop over histograms
 allKey = [x.GetName() for x in fIn.GetListOfKeys()]
 chi2max = 0.
-ikey = 0
 
+ikey = 0
 for key in allKey:
     triplet = key.replace('radlength','')
     #if triplet != 'pixel2': continue
@@ -64,21 +63,19 @@ for key in allKey:
  
 
         ######## FIT ############
-        # fit function
         fit = ROOT.TF1("fit","[0]*(1. + [1]*cos(x+[2]) + [3]*cos(2.*x+[4]))",-3.15,3.15);
-        #fit.SetParLimits(1,0.,1.)
-        #fit.SetParLimits(3,0.,1.)
         th1.Fit(fit,"Q0")
-        print '------------------> chi2/ndf = ',round(fit.GetChisquare(),1),'/',fit.GetNDF(),triplet+'_{i}'.format(i=i)
         data[i-1,:] = [th2.GetXaxis().GetBinCenter(i),fit.GetChisquare(),fit.GetParameter(1)]
+
+        # Filter only chi2/ndf > 70 or amp > 10%
         if fit.GetNDF() != 0:
             if (fit.GetChisquare()/float(fit.GetNDF()) < 70.) and (abs(fit.GetParameter(1))<0.1) and (abs(fit.GetParameter(3))<0.1):  continue
+
         fitMC = ROOT.TF1("fitMC","[0]*(1. + [1]*cos(x+[2]) + [3]*cos(2.*x+[4]))",-3.15,3.15);
-        #fitMC.SetParLimits(1,0.,1.)
-        #fitMC.SetParLimits(3,0.,1.)
         th1MC.Fit(fitMC,"Q0")
         #print '---------------MC-----------------> chi2/ndf = ',round(fitMC.GetChisquare(),1),'/',fitMC.GetNDF()
 
+        # Plot DATA and MC together
         ROOT.gStyle.SetOptFit(0000)
         canv0 = ROOT.TCanvas("c0","Canvas0",1000,500)
         canv0.Divide(2,1)
@@ -90,18 +87,14 @@ for key in allKey:
         th1MC.SetLineColor(3)
         th1MC.Draw('same')
 
-        #canv0.cd(2)
-        #th1diff = (th1MC-th1)/th1MC
-        #th1diff.Draw()
         canv0.SaveAs('July13_filtered_'+triplet+'_test'+str(i)+'_combined.png')
         canv0.Close()
         
-
+        # Plot DATA and MC separately with fit
         ROOT.gStyle.SetOptFit(111)
         th1.GetFunction('fit').ResetBit(ROOT.TF1.kNotDraw);
         th1MC.GetFunction('fitMC').ResetBit(ROOT.TF1.kNotDraw);
 
-        # draw
         canv = ROOT.TCanvas("c1","Canvas",1000,500)
         canv.Divide(2,1)
 
@@ -121,6 +114,7 @@ for key in allKey:
     #if ikey > 2: sys.exit()
     continue
 
+    # Plot Chi2
     plt.plot(data[:,0],data[:,1],'*')
     plt.xlabel('$\eta$')
     plt.ylabel('$\chi^2$')
